e54f49cac1916b5af44dda1b7e0d15d152fde116
PPM <pyaephyomg.ppm06@gmail.com>
2026-05-18 12:55:59 +0700

    fix main

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
-// src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
-  console.log(`🚀 Server running on port ${process.env.PORT ?? 3000}`);
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
+
+  console.log(`Server running on port ${port}`);
 }
-void bootstrap();
+
+bootstrap();
